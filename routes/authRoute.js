7737ae627b3e92979b00b33df75306a455f1e21e
papecode <papesaliouwade500@gmail.com>
2024-05-16 15:05:24 +0000

    remove emoji

--- routes/authRoute.js
+++ routes/authRoute.js
@@ -4,7 +4,7 @@ const authController = require('../controllers/authController');
 const router = express.Router();
 
 router.get('/', (req, res) => {
-    res.send('Bienvenue dans votre api d\'authentification 😎');
+    res.send('Bienvenue dans votre api d\'authentification');
 });
 router.post('/signup', authController.signup);
 router.post('/login', authController.login);
